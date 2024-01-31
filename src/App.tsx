f890f9054aea8e61743a3cd1c724b1b4c3f0b88a
gb <13611993+netalkGB@users.noreply.github.com>
2024-02-01 00:52:42 +0900

    Reducerを移動し、initTracksメソッドを移動した

--- src/App.tsx
+++ src/App.tsx
@@ -1,98 +1,11 @@
 import React, { useEffect, useReducer } from 'react'
 import './App.css'
-import Track from './Track.ts'
 import Monitor from './components/monitor/Monitor.tsx'
 import MidiInputDevicesSelector from './components/deviceselector/MidiInputDevicesSelector.tsx'
 import MidiOutputDevicesSelector from './components/deviceselector/MidiOutputDevicesSelector.tsx'
 import { compareArray } from './utils/ArrayUtils.ts'
 import { checkSysExChecksum } from './utils/GsUtils.ts'
-
-interface State {
-  midiInputs: Array<[string, WebMidi.MIDIInput]> | null
-  midiOutputs: Array<[string, WebMidi.MIDIOutput]> | null
-  activeMidiInput: WebMidi.MIDIInput | null
-  activeMidiOutput: WebMidi.MIDIOutput | null
-  force55Map: boolean
-  tracks: Track[]
-}
-
-type Action = | {
-  type: 'setMidiInputs'
-  payload: Array<[string, WebMidi.MIDIInput]> | null
-} | {
-  type: 'setMidiOutputs'
-  payload: Array<[string, WebMidi.MIDIOutput]> | null
-} | {
-  type: 'setActiveMidiInput'
-  payload: WebMidi.MIDIInput | null
-} | {
-  type: 'setActiveMidiOutput'
-  payload: WebMidi.MIDIOutput | null
-} | {
-  type: 'toggleForce55Map'
-  payload: boolean
-} | {
-  type: 'setTrack'
-  payload: { channel: number
-    track: Track }
-}
-
-const initialState = {
-  midiInputs: null,
-  midiOutputs: null,
-  activeMidiInput: null,
-  activeMidiOutput: null,
-  force55Map: false,
-  tracks: createInitialTracks()
-}
-
-function createInitialTracks (): Track[] {
-  const tracks: Track[] = []
-  for (let i = 0; i < 16; i++) {
-    tracks.push(Track.makePart({ bankMSB: 0, bankLSB: 0, programNumber: 0, channel: i + 1 }))
-  }
-  return tracks
-}
-
-const reducer = (state: State, action: Action): State => {
-  switch (action.type) {
-    case 'setMidiInputs':
-      return {
-        ...state,
-        midiInputs: action.payload
-      }
-    case 'setMidiOutputs':
-      return {
-        ...state,
-        midiOutputs: action.payload
-      }
-    case 'setActiveMidiInput':
-      return {
-        ...state,
-        activeMidiInput: action.payload
-      }
-    case 'setActiveMidiOutput':
-      return {
-        ...state,
-        activeMidiOutput: action.payload
-      }
-    case 'toggleForce55Map':
-      return {
-        ...state,
-        force55Map: action.payload
-      }
-    case 'setTrack':
-      // eslint-disable-next-line no-case-declarations
-      const tracks = state.tracks
-      tracks[action.payload.channel] = action.payload.track
-      return {
-        ...state,
-        tracks
-      }
-    default:
-      return state
-  }
-}
+import { initialState, reducer } from './state/reducer.ts'
 
 function App (): React.ReactNode {
   const [state, dispatch] = useReducer(reducer, initialState)
@@ -165,6 +78,11 @@ function App (): React.ReactNode {
       })
     }
 
+    const initTracks = (): void => {
+      state.tracks.forEach((track, _i) => {
+        track.reset()
+      })
+    }
     activeMidiInput.onmidimessage = (event: WebMidi.MIDIMessageEvent) => { // FIXME: Consider using addEventListener
       const data = event.data
 
@@ -294,13 +212,6 @@ function App (): React.ReactNode {
 
       activeMidiOutput.send(data)
     }
-  }
-
-  // commitやdispatchにあったものはいったんここへ
-  function initTracks (): void {
-    state.tracks.forEach((track, _i) => {
-      track.reset()
-    })
   }
 
   return (
